1f6f050719344d7ff9e6e1e55ac159ad47918347
MilenaMaldonado <105258677+MilenaMaldonado@users.noreply.github.com>
2023-05-12 00:46:44 -0500

    Create Fraccion.cpp

---
+++ Deber2/Fraccion.cpp
@@ -0,0 +1,90 @@
+
+/***********************************************************************
+Universidad de las Fuerzas Armadas ESPE
+AUTORES: Alejandro Cuadrado, Milena Maldonado, Jefferson Ulco
+FECHA DE CREACIÓN: 11/05/2023
+FECHA DE MODIFICACIÓN: 11/05/2023
+Enunciado
+ ***********************************************************************/
+
+
+#include "Fraccion.h"
+//#include "Proceso.h"
+
+Fraccion::Fraccion(){}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Fraccion::getNumerador()
+// Purpose:    Implementation of Fraccion::getNumerador()
+// Return:     float
+////////////////////////////////////////////////////////////////////////
+
+float Fraccion::getNumerador(void)
+{
+   return numerador;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Fraccion::setNumerador(float newNumerador)
+// Purpose:    Implementation of Fraccion::setNumerador()
+// Parameters:
+// - newNumerador
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Fraccion::setNumerador(float newNumerador)
+{
+   numerador = newNumerador;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Fraccion::getDenominador()
+// Purpose:    Implementation of Fraccion::getDenominador()
+// Return:     float
+////////////////////////////////////////////////////////////////////////
+
+float Fraccion::getDenominador(void)
+{
+   return denominador;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Fraccion::setDenominador(float newDenominador)
+// Purpose:    Implementation of Fraccion::setDenominador()
+// Parameters:
+// - newDenominador
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Fraccion::setDenominador(float newDenominador)
+{
+   denominador = newDenominador;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Fraccion::Fraccion(float num, float den)
+// Purpose:    Implementation of Fraccion::Fraccion()
+// Parameters:
+// - num
+// - den
+// Return:     
+////////////////////////////////////////////////////////////////////////
+
+Fraccion::Fraccion(float num, float den)
+{
+   // TODO : implement
+   this->numerador=num;
+   this->denominador=den;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Fraccion::~Fraccion()
+// Purpose:    Implementation of Fraccion::~Fraccion()
+// Return:     
+////////////////////////////////////////////////////////////////////////
+
+Fraccion::~Fraccion()
+{
+   // TODO : implement
+}
+
