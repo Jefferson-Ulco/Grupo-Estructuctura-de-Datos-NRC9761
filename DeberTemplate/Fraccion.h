6a1d53047f6d195210b1cc54e336fa7c6535e4f6
MilenaMaldonado <105258677+MilenaMaldonado@users.noreply.github.com>
2023-05-15 01:18:09 -0500

    Create Fraccion.h

---
+++ DeberTemplate/Fraccion.h
@@ -0,0 +1,34 @@
+
+/***********************************************************************
+Universidad de las Fuerzas Armadas ESPE
+AUTORES: Alejandro Cuadrado, Milena Maldonado, Jefferson Ulco
+FECHA DE CREACIÓN: 13/05/2023
+FECHA DE MODIFICACIÓN: 14/05/2023
+Enunciado
+ ***********************************************************************/
+
+
+#if !defined(__Class_Diagram_1_Fraccion_h)
+#define __Class_Diagram_1_Fraccion_h
+
+template <typename T>
+class Fraccion
+{
+public:
+	Fraccion();
+   T getNumerador(void);
+   void setNumerador(T newNumerador);
+   T getDenominador(void);
+   void setDenominador(T newDenominador);
+   Fraccion(T num, T den);
+  // T calculo(Fraccion obj1, Fraccion obj2);
+   
+   ~Fraccion();
+
+protected:
+private:
+   T numerador;
+   T denominador;
+};
+
+#endif
