e429e96a9ca021e0090fa1b15966437328e01379
MilenaMaldonado <105258677+MilenaMaldonado@users.noreply.github.com>
2023-05-15 01:22:30 -0500

    Update Proceso.h

--- DeberTemplate/Proceso.h
+++ DeberTemplate/Proceso.h
@@ -1,1 +1,28 @@
 
+
+/***********************************************************************
+Universidad de las Fuerzas Armadas ESPE
+AUTORES: Alejandro Cuadrado, Milena Maldonado, Jefferson Ulco
+FECHA DE CREACIÓN: 11/05/2023
+FECHA DE MODIFICACIÓN: 11/05/2023
+Enunciado
+ ***********************************************************************/
+
+#if !defined(__Class_Diagram_1_Proceso_h)
+#define __Class_Diagram_1_Proceso_h
+
+#include "Fraccion.h"
+template <typename T>
+class Proceso
+{
+public:
+
+   virtual Fraccion<T> calculo(Fraccion<T>* obj1, Fraccion<T>* obj2)=0;
+   virtual T calculo(Fraccion<T> obj1, Fraccion<T> obj2) = 0;
+
+protected:
+private:
+
+};
+
+#endif
