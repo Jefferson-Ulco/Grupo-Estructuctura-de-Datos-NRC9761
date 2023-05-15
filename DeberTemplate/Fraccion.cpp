4277be2c3595787f377f846ab6e85ff7bda472ca
MilenaMaldonado <105258677+MilenaMaldonado@users.noreply.github.com>
2023-05-15 01:18:34 -0500

    Create Fraccion.cpp

---
+++ DeberTemplate/Fraccion.cpp
@@ -0,0 +1,62 @@
+
+/***********************************************************************
+Universidad de las Fuerzas Armadas ESPE
+AUTORES: Alejandro Cuadrado, Milena Maldonado, Jefferson Ulco
+FECHA DE CREACIÓN: 11/05/2023
+FECHA DE MODIFICACIÓN: 11/05/2023
+Enunciado
+ ***********************************************************************/
+
+
+#include "Fraccion.h"
+//#include "Proceso.h"
+
+template <typename T>
+Fraccion<T>::Fraccion(){}
+
+
+template <typename T>
+T Fraccion<T>::getNumerador(void)
+{
+   return numerador;
+}
+
+template <typename T>
+void Fraccion<T>::setNumerador(T newNumerador)
+{
+   numerador = newNumerador;
+}
+
+
+template <typename T>
+T Fraccion<T>::getDenominador(void)
+{
+   return denominador;
+}
+
+template <typename T>
+void Fraccion<T>::setDenominador(T newDenominador)
+{
+   denominador = newDenominador;
+}
+
+
+template <typename T>
+Fraccion<T>::Fraccion(T num, T den)
+{
+   // TODO : implement
+   this->numerador=num;
+   this->denominador=den;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Fraccion::~Fraccion()
+// Purpose:    Implementation of Fraccion::~Fraccion()
+// Return:     
+////////////////////////////////////////////////////////////////////////
+template <typename T>
+Fraccion<T>::~Fraccion()
+{
+   // TODO : implement
+}
+
