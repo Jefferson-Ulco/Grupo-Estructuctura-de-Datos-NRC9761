b33d6aa9aba9ea22a7123a43121e72955570ef5f
MilenaMaldonado <105258677+MilenaMaldonado@users.noreply.github.com>
2023-05-17 08:54:59 -0500

    Update Operadores.h

--- SobreCargaOperadores/Operadores.h
+++ SobreCargaOperadores/Operadores.h
@@ -1,21 +1,20 @@
+#if !defined(operadores_h)
+#define operadores_h
+#include <iostream>
 #include <stdlib.h>
 #include <conio.h>
 #include <stdio.h>
-#include <iostream>
-#if !defined(__SobreCargaDeDatos_Operadores_h)
-#define __SobreCargaDeDatos_Operadores_h
-
-
+using namespace std;
 class Operadores
 {
 private:
    int x;
 public:
-   Operadores operator%(const Operadores &m) ;
-
+   Operadores(const int  x);
+   Operadores operator %( Operadores& m)const ;
    int getX(void);
    void setX(int newX);
-	Operadores(const int  x);
+	
 protected:
 };
 
