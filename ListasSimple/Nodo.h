6d4168b4d640488c198c301eb1f3b2780924ec51
MilenaMaldonado <105258677+MilenaMaldonado@users.noreply.github.com>
2023-05-30 23:21:33 -0500

    Update Nodo.h

--- ListasSimple/Nodo.h
+++ ListasSimple/Nodo.h
@@ -1,12 +1,25 @@
+/***********************************************************************
+Universidad de las Fuerzas Armadas ESPE
+AUTORES: Alejandro Cuadrado, Milena Maldonado, Jefferson Ulco
+FECHA DE CREACIÓN: 27/05/2023
+FECHA DE MODIFICACIÓN: 30/05/2023
+Realizar el insertar por cola de una lista simple
+ ***********************************************************************/
+
+#include <iostream>
 #pragma once
-class Nodo{
-	private:
-		int valor;
-		Nodo *siguiente;
-	public:
-		Nodo(int val, Nodo *sig=NULL){
-			this->valor=val;
-			this->siguiente=sig;
-		}
-	friend class Lista;
+
+class Nodo {
+private:
+    int valor;
+    Nodo* siguiente;
+
+public:
+    Nodo(int val, Nodo* sig = nullptr);
+    ~Nodo();
+    void setValor(int val);
+    void setSiguiente(Nodo *sig);
+	int getValor();
+    Nodo *getSiguiente();
+    friend class Lista;
 };
