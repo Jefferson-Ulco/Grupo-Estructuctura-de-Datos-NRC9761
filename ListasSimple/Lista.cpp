2b960931c08e1d501cfcd6293405c1c8a500cb66
MilenaMaldonado <105258677+MilenaMaldonado@users.noreply.github.com>
2023-05-30 23:21:03 -0500

    Update Lista.cpp

--- ListasSimple/Lista.cpp
+++ ListasSimple/Lista.cpp
@@ -1,42 +1,119 @@
-
-/***********************************************************************
-Universidad de las Fuerzas Armadas ESPE
-AUTORES: Alejandro Cuadrado, Milena Maldonado, Jefferson Ulco
-FECHA DE CREACIÓN: 29/05/2023
-FECHA DE MODIFICACIÓN: 29/05/2023
-Enunciado
- ***********************************************************************/
-
-#include <iostream>
 #include "Lista.h"
-bool Lista::listaVacia(){
-			return (this->primero==NULL);
-		}
+#include <iostream>
 
-Lista::Lista(){
-			this->primero=NULL;
-			this->actual=NULL;
-		}
+bool Lista::listaVacia() {
+    return (primero == nullptr);
+}
 
-void Lista:: insertar(int val){
-		Nodo *nuevo=new Nodo(val,NULL);
-		if (listaVacia()){
-			this->primero=nuevo;
-		}
-		else{
-			this->actual->siguiente=nuevo;
-		}
-		this->actual=nuevo;
-	}
-	
-	
-void Lista:: mostrarLista(){
-		Nodo *tmp=this->primero;
-		while(tmp){
-			cout<<tmp->valor<<"-->";
-			tmp=tmp->siguiente;
-		}
-		cout<<"NULL";
-	}
+Lista::Lista() {
+    primero = nullptr;
+    actual = nullptr;
+}
 
+void Lista::insertar(int val) {
+    Nodo* nuevo = new Nodo(val);
+    if (listaVacia()) {
+        primero = nuevo;
+    } else {
+        nuevo->siguiente = primero;
+    }
+    primero = nuevo;
+}
+
+void Lista::insertarCola(int val) {
+    Nodo* nuevo = new Nodo(val);
+    if (listaVacia()) {
+        primero = nuevo;
+    } else {
+        actual->siguiente = nuevo;
+    }
+    actual = nuevo;
+}
+void Lista::eliminar() {
+    Nodo* aux = primero;
+    if (listaVacia()) {
+        std::cout << "La lista esta vacía" << std::endl;
+    } else {
+        primero=primero->siguiente;
+        delete aux;
+    }
+   
+}
+
+void Lista::eliminarCola() {
+    Nodo* aux = primero;
+    if (listaVacia()) {
+        std::cout << "La lista esta vacía" << std::endl;
+    } else {
+         while (aux->siguiente != actual)
+            {
+                aux = aux->siguiente;
+            }
+            delete actual;
+            actual= aux;
+            actual->siguiente= NULL;
+    }
+   
+}
+
+void Lista::mostrarLista() {
+    Nodo* tmp = primero;
+    	std::cout << "\n/////////////////////////////////\n\n" ;
+    while (tmp) {
+    
+        std::cout << tmp->valor << "-->";
+        tmp = tmp->siguiente;
+    }
+    std::cout << "NULL";
+       std::cout << "\n\n/////////////////////////////////\n" ;
+}
+
+void Lista::mostrarMenu() {
+    int opcion = 0;
+    int valor = 0;
+
+    while (opcion != 6) {
+        std::cout << "----- MENU -----" << std::endl;
+        std::cout << "1. Insertar al principio" << std::endl;
+        std::cout << "2. Insertar al final" << std::endl;
+        std::cout << "3. Eliminar al principio" << std::endl;
+        std::cout << "4. Eliminar al final" << std::endl;
+        std::cout << "5. Mostrar lista" << std::endl;
+        std::cout << "6. Salir" << std::endl;
+        std::cout << "Ingrese una opcion: ";
+        std::cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                std::cout << "Ingrese el valor a insertar al principio: ";
+                std::cin >> valor;
+                insertar(valor);
+                break;
+            case 2:
+                std::cout << "Ingrese el valor a insertar al final: ";
+                std::cin >> valor;
+                insertarCola(valor);
+                break;
+            case 3:
+                std::cout << "Eliminando el primer nodo\n ";
+                eliminar();
+                break;
+            case 4:
+                std::cout << "Eliminando el ultimo nodo\n ";
+                eliminarCola();
+                break;
+            case 5:
+                std::cout << "Lista: ";
+                mostrarLista();
+                std::cout << std::endl;
+                break;
+            case 6:
+                std::cout << "Saliendo..." << std::endl;
+                break;
+            default:
+                std::cout << "Opción invalida. Intente nuevamente." << std::endl;
+                break;
+        }
+    }
+}
 
