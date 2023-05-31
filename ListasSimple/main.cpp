6fae3f7211a49bd833233e9b136287a7f5beb4fa
MilenaMaldonado <105258677+MilenaMaldonado@users.noreply.github.com>
2023-05-30 23:22:47 -0500

    Update main.cpp

--- ListasSimple/main.cpp
+++ ListasSimple/main.cpp
@@ -1,17 +1,18 @@
+/***********************************************************************
+Universidad de las Fuerzas Armadas ESPE
+AUTORES: Alejandro Cuadrado, Milena Maldonado, Jefferson Ulco
+FECHA DE CREACIÓN: 27/05/2023
+FECHA DE MODIFICACIÓN: 30/05/2023
+Realizar el insertar por cola de una lista simple
+ ***********************************************************************/
+
 #include <iostream>
-#include "Lista.cpp"
+#include "Lista.h"
+
 int main(int argc, char** argv) {
-	Lista lst;
-	lst.insertar(5);
-	lst.insertar(15);
-	lst.insertar(25);
-	lst.insertar(35);
-	lst.insertar(45);
-	lst.insertar(55);
-	lst.insertar(65);
-	lst.insertar(57);
-	lst.insertar(85);
-	lst.insertar(95);
-	lst.mostrarLista();
-	return 0;
+    Lista lst;
+
+    lst.mostrarMenu();
+
+    return 0;
 }
