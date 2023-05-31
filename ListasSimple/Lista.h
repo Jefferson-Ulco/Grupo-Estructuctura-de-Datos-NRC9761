7fbb993380845b647584028fb590e848ae3bd382
MilenaMaldonado <105258677+MilenaMaldonado@users.noreply.github.com>
2023-05-30 23:21:52 -0500

    Update Lista.h

--- ListasSimple/Lista.h
+++ ListasSimple/Lista.h
@@ -1,15 +1,29 @@
+/***********************************************************************
+Universidad de las Fuerzas Armadas ESPE
+AUTORES: Alejandro Cuadrado, Milena Maldonado, Jefferson Ulco
+FECHA DE CREACIÓN: 27/05/2023
+FECHA DE MODIFICACIÓN: 30/05/2023
+Realizar el insertar por cola de una lista simple
+ ***********************************************************************/
+
 #pragma once
+
 #include "Nodo.h"
-#include <iostream>
-using namespace std;
-class Lista{
-	private:
-		Nodo *primero;
-		Nodo *actual;
-	
-	public:
-		Lista();
-	bool listaVacia();
-	void insertar(int val);
-	void mostrarLista();  
-};                   
+
+class Lista {
+private:
+    Nodo* primero;
+    Nodo* actual;
+
+    bool listaVacia();
+
+public:
+    Lista();
+    void insertar(int val);
+    void insertarCola(int val);
+    void eliminar();
+    void eliminarCola();
+    void mostrarLista();
+    void mostrarMenu();
+
+};
